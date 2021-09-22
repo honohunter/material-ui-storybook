04d4ad43b0a57c8e86d81f704c755efc496bfaef
Ala Eddine Bouasla <bbbooo1994@gmail.com>
2021-09-22 09:47:30 +0100

    Fix

--- src/buttons/button/index.stories.tsx
+++ src/buttons/button/index.stories.tsx
@@ -8,15 +8,22 @@ export default {
   component: Button,
   argTypes: {
     backgroundColor: { control: 'color' },
+    size: {
+      options: ['large', 'medium', 'small'],
+      control: { type: 'radio' },
+    },
   },
 } as Meta;
 
 // Create a master template for mapping args to render the Button component
 const Template: Story<ButtonProps> = args => <Button {...args} />;
 
+export const Default = Template.bind({});
+Default.args = { label: 'Default', size: 'medium' };
+
 // Reuse that template for creating different stories
 export const Primary = Template.bind({});
-Primary.args = { label: 'Primary 😃', size: 'large' };
+Primary.args = { label: 'Primary 😃', size: 'medium' };
 
 export const Secondary = Template.bind({});
 Secondary.args = { ...Primary.args, primary: false, label: 'Secondary 😇' };
